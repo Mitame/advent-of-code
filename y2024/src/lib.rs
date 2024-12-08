668edc8152994a60a54315a267692fa20a08715e
Nanashi Mitame <gitcommits@mita.me>
2024-12-08 00:32:27 +0000

    Add framework for problems

--- y2024/src/lib.rs
+++ y2024/src/lib.rs
@@ -1,1 +1,20 @@
+#![feature(iterator_try_collect)]
+use std::io;
 
+type Solver = fn(&mut dyn io::Read);
+
+pub struct Aoc {
+    pub day: usize,
+    pub part: usize,
+    pub solver: Solver,
+}
+
+impl Aoc {
+    const fn new(day: usize, part: usize, solver: Solver) -> Aoc {
+        Aoc { day, part, solver }
+    }
+}
+
+inventory::collect!(Aoc);
+
+include!(concat!(env!("OUT_DIR"), "/days.rs"));
---
+++ y2024/src/main.rs
@@ -0,0 +1,27 @@
+use std::{
+    io::stdin,
+    path::PathBuf, process::exit,
+};
+
+use advent_of_code::Aoc;
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+struct Args {
+    day: usize,
+    part: Option<usize>,
+    input: Option<PathBuf>,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    for solution in inventory::iter::<Aoc> {
+        if solution.day == args.day && args.part.map(|part| part == solution.part).unwrap_or(true) {
+            return (solution.solver)(&mut stdin())
+        }
+    }
+
+    eprintln!("Could not find solution for d{} p{:?}", args.day, args.part);
+    exit(1)
+}
