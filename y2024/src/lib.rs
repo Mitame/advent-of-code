63a6f89cdea03c52439a6b7c945e142204ae2bff
Nanashi Mitame <gitcommits@mita.me>
2024-12-08 11:18:20 +0000

    Remove the build.rs hacks

--- y2024/src/lib.rs
+++ y2024/src/lib.rs
@@ -17,4 +17,11 @@ impl Aoc {
 
 inventory::collect!(Aoc);
 
-include!(concat!(env!("OUT_DIR"), "/days.rs"));
+pub mod day01;
+pub mod day02;
+pub mod day03;
+pub mod day04;
+pub mod day05;
+pub mod day06;
+pub mod day07;
+pub mod day08;
